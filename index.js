d9e4ea32355d365dc804810d08d8c65cc480cd6a
Swarnavo Paul <swarnavo.paul2@mindtree.com>
2021-02-04 15:52:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let request=require('request');
+
+// request("https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",function(err,response,body){
+//     let bodyJSON=JSON.parse(body);
+//     let randomQuote=bodyJSON[0]["content"];
+//     document.getElementById('quote').innerHTML=randomQuote;
+// })
+
+document.getElementById('quote').innerHTML="Hello";
---
+++ main.js
@@ -0,0 +1,40 @@
+const electron=require('electron');
+const app=electron.app;
+const BrowserWindow=electron.BrowserWindow;
+const path=require('path');
+const url=require('url');
+
+// let win;
+
+// function createWindow(){
+//     win=new BrowserWindow();
+//     win.loadURL(url.format({
+//         pathname:path.join(__dirname,'index.html'),
+//         protocol:'file',
+//         slashes:true
+//     }));
+
+//     win.on('closed',()=>{
+//         win=null;
+//     })
+// }
+
+// app.on('ready',createWindow);
+
+let win;
+
+function createWindow(){
+    win=new BrowserWindow({height:150,width:500});
+    win.loadURL(url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol:'file',
+        slashes:true
+    }));
+
+    win.on('closed',()=>{
+        win=null;
+    })
+}
+
+app.on('ready',createWindow);
+
